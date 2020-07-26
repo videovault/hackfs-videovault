3c694dec92b37316e249b3a3a62d567218fd1dd6
KalpitaMandal <49894180+KalpitaMandal@users.noreply.github.com>
2020-07-27 00:47:36 +0530

    Add files via upload

---
+++ test/Token.js
@@ -0,0 +1,73 @@
+const { expect } = require("chai");
+
+describe("Token contract", function() {
+
+    let Token;
+    let buidlerToken;
+    let TokenInstance;
+    let owner;
+    let addr1;
+    let addr2;
+    let addr3;
+
+    beforeEach(async function () {
+        Token = await ethers.getContractFactory("nft");
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        buidlerToken = await Token.deploy();
+        TokenInstance = await buidlerToken.deployed();
+
+        await buidlerToken.deployed();
+    });
+    
+    describe("Token deployment", function() {
+        it("Token contract should be deployed successfully", async function() {
+            expect(TokenInstance.address).to.not.equal('');
+            expect(TokenInstance.address).to.not.equal(null);
+            expect(TokenInstance.address).to.not.equal(undefined);
+            expect(TokenInstance.address).to.not.equal(0x0);
+        });
+        
+        it("Token deployed should have a name", async function() {
+            expect(await buidlerToken.name()).to.equal("Video-Vault");
+        });
+        
+        it("Token deployed should have a symbol", async function() {
+            expect(await buidlerToken.symbol()).to.equal("VIVA");
+        });
+    });
+
+    describe("Adding content creators and consumers", function() {
+    
+        it("Should add content creators successfully", async function() {
+            await buidlerToken.AccountCredentials("Emily");
+            [UserID, Username, Useraddress] = await TokenInstance.creators(0);
+            // console.log(UserID);
+            // console.log(Username);
+            // console.log(Useraddress);
+            expect(UserID.toNumber()).to.equal(1);
+            expect(Username).to.equal("Emily");
+            expect(Useraddress).to.equal(await owner.getAddress());
+        });
+
+        it("Should add subscribers successfully", async function() {
+            await buidlerToken.connect(addr1).Subscribe("Frank");
+            [SubsID, Subsname, Subsaddress] = await TokenInstance.subscribers(0);
+            // console.log(SubsID);
+            // console.log(Subsname);
+            // console.log(Subsaddress);
+            expect(SubsID.toNumber()).to.equal(1);
+            expect(Subsname).to.equal("Frank");
+            expect(Subsaddress).to.equal(await addr1.getAddress());
+        });
+    });
+
+    describe("Should successfully make association between content creators and subscribers", function() {
+
+        // it("Associate the subscribers to the correct content creators and vice versa", async function() {
+        //     await buidlerToken.AccountCredentials("George");
+        //     await buidlerToken.connect(addr2).Subscribe("Amy");
+        //     const result = await buidlerToken.Association(0,[0,1]);
+        //     console.log(result);
+        // });
+    });
+});
